f5acefe811ef68ef153db96cdcf2cc71e9fcbea5
Elina Hotman <elina.hotman@coremedia.com>
2014-06-07 20:33:45 +0200

    First draft of UI wireframe

--- left4u/gen/de/hackthon/left4u/R.java
+++ left4u/gen/de/hackthon/left4u/R.java
@@ -982,6 +982,7 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int screen1=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -998,13 +999,13 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int browse_button=0x7f05003e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
-        public static final int dummy_button=0x7f05003e;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1016,12 +1017,15 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imgLogo=0x7f050041;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int map_button=0x7f05003f;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int post_button=0x7f050040;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1098,7 +1102,8 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_fullscreen=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_splash=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1142,8 +1147,11 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
-        public static final int dummy_button=0x7f0a000e;
+        public static final int browse_button=0x7f0a000e;
         public static final int dummy_content=0x7f0a000f;
+        public static final int map_button=0x7f0a0010;
+        public static final int next_button=0x7f0a0012;
+        public static final int post_button=0x7f0a0011;
     }
     public static final class style {
         /** 
--- left4u/src/de/hackthon/left4u/FullscreenActivity.java
+++ left4u/src/de/hackthon/left4u/FullscreenActivity.java
@@ -111,7 +111,10 @@ public class FullscreenActivity extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+//        findViewById(R.id.next_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.browse_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.map_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.post_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
